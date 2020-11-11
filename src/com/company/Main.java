50ba196d29b5b26a0f643e6424b9ebadef933027
MSarandev <mar7insarandev@gmail.com>
2020-11-11 14:59:53 +0200

    code changed, correct sytaxsis

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.Out.Printline{"First change made from gitHub acc"}
+	System.out.println("First change made from gitHub acc");
+	// your syntax's not good
     }
 }
