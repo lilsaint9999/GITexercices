73cef6a86fc9ec93a328f3b2dccccecbb4576dde
lilsaint9999 <61825146+lilsaint9999@users.noreply.github.com>
2020-11-11 15:02:46 +0200

    add a new //comment @main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("First change made from gitHub acc");
 	// your syntax's not good
+	// yes now its correct , keep learning from Udemy!!!!
     }
 }
