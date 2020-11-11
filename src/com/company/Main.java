86698d1558682fd8b1b47244c6916f85d5abe0a3
lilsaint9999 <61825146+lilsaint9999@users.noreply.github.com>
2020-11-11 14:33:01 +0200

    update main.java
    check if errors presents

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.Out.Printline{"First change made from gitHub acc"}
     }
 }
